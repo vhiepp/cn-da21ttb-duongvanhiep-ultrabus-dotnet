6f4b1139479cb9a5a053b9349847d66c203770d8
Văn Hiệp <dhiep2307@gmail.com>
2024-10-31 13:22:12 +0700

    up folder src

---
+++ src/UltraBusAPI/UltraBusAPI/Entities/Account.cs
@@ -0,0 +1,6 @@
+﻿namespace UltraBusAPI.Entities
+{
+    public class Account
+    {
+    }
+}
---
+++ src/UltraBusAPI/UltraBusAPI/Entities/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UltraBusAPI.Entities
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
