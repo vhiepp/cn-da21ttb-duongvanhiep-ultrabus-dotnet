67f6c859e341d350c480d69a77f22bd282655d77
Văn Hiệp <dhiep2307@gmail.com>
2025-01-07 19:46:33 +0700

    update news

--- src/ultrabus-web/src/pages/news.jsx
+++ src/ultrabus-web/src/pages/news.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import SEO from '../common/seo';
-import Blog from '../components/blog';
-import Wrapper from '../layout/wrapper';
+import React from "react";
+import SEO from "../common/seo";
+import Blog from "../components/blog";
+import Wrapper from "../layout/wrapper";
 
 const index = () => {
-    return (
-        <Wrapper>
-            <SEO pageTitle={"Softec - Data analytics"} /> 
-            <Blog />           
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <SEO pageTitle={"Tin tức mới nhất"} />
+      <Blog />
+    </Wrapper>
+  );
 };
 
 export default index;
